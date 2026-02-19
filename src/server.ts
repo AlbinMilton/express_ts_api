abcdb9d5d99c29a1aeba93cd414578d6b63d12fb
Albin Milton <albinmltn@gmail.com>
2026-02-19 11:49:44 +0100

    Connected express with psql

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import dotenv from "dotenv";
 import { createApp } from "./app";
+import { pool } from "./config/db";
 
 dotenv.config();
 const PORT = process.env.PORT ? Number(process.env.PORT) : 4000;
 const app = createApp();
-
+pool
+  .connect()
+  .then(() => {
+    console.log("✅ DB Connected");
+  })
+  .catch((e) => {
+    console.error(e);
+  });
 app.listen(PORT, () => {
-  console.log(`😩Server is running on http://localhost:${PORT}`);
+  console.log(`🚀✅Server is running on http://localhost:${PORT}`);
 });
